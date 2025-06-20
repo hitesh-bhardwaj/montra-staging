b6d5d697524cf64cd4aefdfc3ede0e1dd8d81012
Vidushi Saxena <vidushisaxenabdn786@gmail.com>
2025-06-20 20:01:47 +0530

    update

--- src/components/Metadata/index.js
+++ src/components/Metadata/index.js
@@ -45,5 +45,20 @@ export function generateMetadata({
       description,
       images: [imageUrl],
     },
+    robots: {
+      index: true,
+      follow: true,
+      nocache: false,
+      googleBot: {
+        index: true,
+        follow: true,
+        noimageindex: false,
+        'max-video-preview': -1,
+        'max-image-preview': 'large',
+        'max-snippet': -1,
+      },
+    },
+  
   };
+  
 }
