0909f5bf3541b5be5849d20a8c9fa8505d5c1d85
Gaurav Verma <briskgaurav@gmail.com>
2025-06-27 16:43:11 +0530

    Update
    Privacy Policy Page

---
+++ src/app/privacy-policy/page.jsx
@@ -0,0 +1,13 @@
+import Content from '@/components/Privacy Policy/Content'
+import Hero from '@/components/Privacy Policy/Hero'
+import React from 'react'
+
+export default function page() {
+  return (
+  <>
+  <Hero />
+  <Content />
+  </>
+
+  )
+}
---
+++ src/components/Privacy Policy/Hero.jsx
@@ -0,0 +1,93 @@
+'use client'
+import React, { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function Hero() {
+  useEffect(() => {
+
+    const ctx = gsap.context(() => {
+
+      gsap.set(".montra-logo", {
+        filter: "brightness(16)",
+      });
+      gsap.to(".montra-logo", {
+        filter: "brightness(16)",
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          // markers: true,
+      
+          onEnter: () => {
+            gsap.to(".montra-logo", {
+              filter: "brightness(16)",
+              duration: 0,
+            });
+          },
+          onLeave: () => {
+            gsap.to(".montra-logo", {
+              filter: "brightness(1)",
+              duration: 0,
+            });
+          },
+          // onLeaveBack: () => {
+          //   gsap.to(".montra-logo", {
+          //     filter: "brightness(1)",
+          //     duration: 0,
+          //   });
+          // },
+          onEnterBack: () => {
+            gsap.to(".montra-logo", {
+              filter: "brightness(16)",
+              duration: 0,
+            });
+          },
+        },
+      });
+      gsap.to(".ham-mobile", {
+        backgroundColor: "#215CFF",
+        duration: 0,
+        scrollTrigger: {
+          trigger: "#steps",
+          start: "top top",
+          // markers: true,
+          onEnter: () => {
+            gsap.to(".ham-mobile", {
+              backgroundColor: "white",
+              duration: 0,
+            });
+          },
+        
+          onLeave: () => {
+            gsap.to(".ham-mobile", {
+              backgroundColor: "#215CFF",
+              duration: 0,
+            });
+          },
+          onEnterBack: () => {
+            gsap.to(".ham-mobile", {
+              backgroundColor: "white",
+              duration: 0,
+            });
+          },
+        },
+      });
+    });
+    return () => ctx.revert();
+  });
+  return (
+    <>
+      <div
+        id="hero"
+        className="h-[70vh] max-sm:h-[45vh]  rounded-b-[3vw] max-md:h-[50vh] max-sm:rounded-b-[9vw] flex items-center justify-center bg-[#215CFF] w-full"
+      >
+        <h1 className="flex flex-col items-center max-sm:text-[10vw] justify-center text-[5.7vw] max-md:text-[7.5vw] font-display font-medium capitalize leading-[1.15] text-white">
+          <span>Montra</span>
+          <span>Privacy Policy</span>
+        </h1>
+      </div>
+    </>
+  );
+}
